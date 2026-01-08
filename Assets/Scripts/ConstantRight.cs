59d019b066b68bf1e499103158d7beda2d65cda3
ZetZin <zetzin0604@gmail.com>
2026-01-08 09:34:59 +0100

    Initial commit

---
+++ Assets/Scripts/ConstantRight.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ConstantRight : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.right * 0.1f;
+    }
+}
